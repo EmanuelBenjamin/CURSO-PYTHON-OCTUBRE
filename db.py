b8ed511a5868d4703afb3271ee6abe900cda5328
Emanuel Dionicio <emanuelbenjamin@hotmail.com>
2022-11-18 00:46:29 -0600

    Exercises on Database and migrations

--- db.py
+++ db.py
@@ -2,19 +2,33 @@
 # UPDATE users (nombre, edad, gereno) VALUES ('Rafael', '10','masculino') WHERE name = 'rafel'
 # SELECT * FROM users WHERE edad = 10 
 
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
 
 Base = declarative_base()
 
 class Alumno(Base):
-    __tablename__ = 'alumno'
+    __tablename__ = 'alumnos'
 
     id = Column(Integer, primary_key=True)
     nombres = Column(String, nullable=False)
     apellidos = Column(String, nullable=False)
     carnet = Column(Integer)
+    notas = relationship('Nota', back_populates='alumno')
 
+    @hybrid_property
+    def nombre_completo(self):
+        return f'{self.nombres} {self.apellidos}'
+
+class Nota(Base):
+    __tablename__ = 'notas'
+
+    id = Column(Integer, primary_key=True)
+    curso = Column(String)
+    nota = Column(Integer)
+    alumno_id = Column(Integer,ForeignKey('alumnos.id'))
+    alumno = relationship('Alumno', back_populates='notas')
 
 engine = create_engine('sqlite:///:memory:')
 
@@ -60,3 +74,20 @@ alumnos = session.query(Alumno).all()
 print(alumnos)
 print(alumnos[0].nombres)
 print(alumnos[1].nombres)
+
+print(luis.nombre_completo)
+
+nota = Nota(
+    curso = 'matematicas',
+    nota = 89,
+    alumno_id=luis.id
+)
+session.add(nota)
+session.commit()
+
+session.refresh(nota)
+print(nota.alumno.nombres)
+session.refresh(luis)
+print(luis.notas[0].nota)
+print(luis.notas[0].curso)
+print(nota.alumno.notas[0].alumno.nombre_completo)
