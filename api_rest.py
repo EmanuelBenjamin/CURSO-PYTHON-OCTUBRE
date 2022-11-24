4c6e68bd4655c5125cfa95f00ed08575eaa2985f
Emanuel Dionicio <emanuelbenjamin@hotmail.com>
2022-11-23 23:26:49 -0600

    Creation of the REST API file

---
+++ api_rest.py
@@ -0,0 +1,18 @@
+#URL https://www.facebook.com/publicaciones
+#Metodo GET -obtener POST - crear PUT - actualizar DELETE - borra
+#Body - JSON
+
+{
+    "nombre": "Rafael",
+    "apellido":"Flores",
+    "altura": 172
+}
+
+#Headers Authorization: abc - 123
+#Path parameters - /publicaciones/1
+#Query params - /publicaciones?owner=5&page=2
+#Response 
+{
+    "message": "Usuario registrado exitosamente"
+}
+#Codigo HTTP
